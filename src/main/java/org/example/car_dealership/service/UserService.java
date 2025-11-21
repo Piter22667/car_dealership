fb9c33957376e0462b2106178e1ba137394c0788
Piter Bilyk <bilykpiter492@gmail.com>
2025-11-21 14:53:28 +0200

    Revert "added base logic for get user details for personal cabinet"
    This reverts commit 672b5ab16428783e8d228e0a458775b5f00bbf39.

--- src/main/java/org/example/car_dealership/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package org.example.car_dealership.service;
-
-import org.example.car_dealership.dto.UserDetailsResponseDto;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserService {
-    UserDetailsResponseDto getUserByEmail(String email);
-}
